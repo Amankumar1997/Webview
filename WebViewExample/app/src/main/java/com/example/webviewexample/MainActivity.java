35fbd8c267c1d6d195652920dbe165268d5a07d1
Aman jainwal <jainwalaman@gmail.com>
2021-01-06 02:15:41 +0200

    webwiew example

---
+++ WebViewExample/app/src/main/java/com/example/webviewexample/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.webviewexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+WebView webView;
+ProgressDialog pd;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView=findViewById(R.id.webview);
+        pd=new ProgressDialog(this);
+
+        pd.setMessage("Loading");
+
+        pd.setTitle("progerss");
+
+        webView.setWebViewClient(new WebViewClient()
+        {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                pd.show();
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                pd.dismiss();
+            }
+        });
+
+
+
+
+
+        webView.loadUrl("https://www.javatpoint.com/android-webview-example");
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack())
+        {
+            webView.goBack();
+        }
+        else
+        {
+            super.onBackPressed();
+        }
+    }
+}
